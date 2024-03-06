6a4f958ffc68c6185aff1b245e2aeca9f5e974ac
Vishal Row Mysore <visrow@gmail.com>
2024-03-05 19:06:22 -0500

    single and multi processor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,78 +9,45 @@ import com.google.cloud.vertexai.generativeai.*;
 import com.t4a.action.BlankAction;
 import com.t4a.api.AIAction;
 import com.t4a.api.JavaMethodExecutor;
+import com.t4a.predict.Predict;
 import com.t4a.predict.PredictionLoader;
+import com.t4a.processor.ActionProcessor;
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
 @Log
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InvocationTargetException, IllegalAccessException {
 
         String projectId = "cookgptserver";
         String location = "us-central1";
         String modelName = "gemini-1.0-pro";
-        String promptText = "My friends name is Vishal , I am wondering what to cook for him today";
-        StringBuffer list = PredictionLoader.getInstance(projectId, location, modelName).getActionNameList();
-        System.out.println(list.toString());
-        try (VertexAI vertexAI = new VertexAI(projectId, location)) {
-            AIAction predictedAction = PredictionLoader.getInstance(projectId, location, modelName).getPredictedAction(promptText);
-            log.info(predictedAction.getActionName());
-            JavaMethodExecutor methodAction = new JavaMethodExecutor();
+        String promptSaveText = "My friends name is Vishal ,he lives in tornto.I want save this info locally ";
+        String cookPromptSingleText = "My friends name is Vishal ,I dont know what to cook for him today.";
+        String cookPromptText = "My friends name is Vishal ,I dont know what to cook for him today. Also I want to take him out to park today";
+        String weatherPrompt = "Hey I am in Toronto do you think i can go out without jacket";
 
-            FunctionDeclaration weatherFunciton = methodAction.buildFunciton(predictedAction);
+        ActionProcessor processor = new ActionProcessor();
+        String result = (String)processor.processSingleAction("cookgptserver","us-central1","gemini-1.0-pro",promptSaveText);
+        log.info(result);
 
-            log.info("Function declaration h1:");
-            log.info("" + weatherFunciton);
+       // List<Object> results = processor.processMultipleAction("cookgptserver","us-central1","gemini-1.0-pro","ey I am in Toronto do you think i can go out without jacket, my friends name is Vinod he lives in Balaghat, please save this information locally ",2);
+        //for (Object resultObj:results
+       // ) {
+         //   log.info((String)resultObj);
 
-            JavaMethodExecutor additionalQuestion = new JavaMethodExecutor();
-            BlankAction blankAction = new BlankAction();
-            FunctionDeclaration additionalQuestionFun = additionalQuestion.buildFunciton(blankAction);
-            log.info("Function declaration h1:");
-            log.info("" + additionalQuestionFun);
-            //add the function to the tool
-            Tool tool = Tool.newBuilder()
-                    .addFunctionDeclarations(weatherFunciton).addFunctionDeclarations(additionalQuestionFun)
-                    .build();
+        //}
 
 
-            GenerativeModel model =
-                    GenerativeModel.newBuilder()
-                            .setModelName(modelName)
-                            .setVertexAi(vertexAI)
-                            .setTools(Arrays.asList(tool))
-                            .build();
-            ChatSession chat = model.startChat();
-
-            log.info(String.format("Ask the question 1: %s", promptText));
-            GenerateContentResponse response = chat.sendMessage(promptText);
-
-            log.info("\nPrint response 1 : ");
-            log.info("" + ResponseHandler.getContent(response));
-            log.info(methodAction.getPropertyValuesJsonString(response));
-
-            Object obj = methodAction.action(response, predictedAction);
-            log.info("" + obj);
-
-            Content content =
-                    ContentMaker.fromMultiModalData(
-                            PartMaker.fromFunctionResponse(
-                                    predictedAction.getActionName(), Collections.singletonMap(predictedAction.getActionName(), obj)));
-
-
-            response = chat.sendMessage(content);
-
-            log.info("Print response content: ");
-            log.info("" + ResponseHandler.getContent(response));
-            log.info(ResponseHandler.getText(response));
-
-        } catch (InvocationTargetException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
---
+++ src/main/java/org/example/WeatherAction.java
@@ -0,0 +1,109 @@
+package org.example;
+
+import com.t4a.api.AIAction;
+import com.t4a.api.ActionType;
+import com.t4a.predict.Predict;
+import lombok.extern.java.Log;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@Log
+@Predict
+public class WeatherAction implements AIAction {
+    public double getTemperature(String cityName) {
+        double temperature = 0;
+        String urlStr = "https://geocoding-api.open-meteo.com/v1/search?name="+cityName+"&count=1&language=en&format=json";
+        String weatherURlStr = "https://api.open-meteo.com/v1/forecast?latitude=";
+        try {
+            StringBuilder response = getResponseFromURl(urlStr);
+
+            // Parse JSON response
+            JSONObject jsonObject  = new JSONObject(response.toString());
+
+            // Extract latitude and longitude from the JSON response
+            JSONArray resultsArray = jsonObject.getJSONArray("results");
+
+            // Extract latitude and longitude from the first element of the "results" array
+            if (resultsArray.length() > 0) {
+                JSONObject result = resultsArray.getJSONObject(0);
+                double latitude = result.getDouble("latitude");
+                double longitude = result.getDouble("longitude");
+
+                // Print latitude and longitude
+                log.info("Latitude: " + latitude);
+                log.info("Longitude: " + longitude);
+
+
+                weatherURlStr = weatherURlStr+latitude+"&longitude="+longitude+"&current=temperature_2m";
+                response = getResponseFromURl(weatherURlStr);
+
+                log.info(response.toString());
+                jsonObject = new JSONObject(response.toString());
+                // Get the "current" object
+                JSONObject currentObject = jsonObject.getJSONObject("current");
+
+                // Extract the temperature value
+                temperature = currentObject.getDouble("temperature_2m");
+
+                // Print the temperature value
+                log.info("Temperature: " + temperature + " °C");
+            } else {
+                log.info("No results found for the longitude and latidue for the city , city is invalid");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return temperature;
+    }
+
+    private static StringBuilder getResponseFromURl(String urlStr) throws IOException {
+        // Create a URL object
+        URL url = new URL(urlStr);
+
+        // Open a connection
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        // Set request method
+        connection.setRequestMethod("GET");
+
+        // Get the response code
+        int responseCode = connection.getResponseCode();
+        log.info("Response Code: " + responseCode);
+
+        // Read the response body
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        // Disconnect the connection
+        connection.disconnect();
+        return response;
+    }
+
+
+
+    @Override
+    public String getActionName() {
+        return "getTemperature";
+    }
+
+    @Override
+    public ActionType getActionType() {
+        return ActionType.JAVAMETHOD;
+    }
+
+    @Override
+    public String getDescription() {
+        return "get weather for city";
+    }
+}
