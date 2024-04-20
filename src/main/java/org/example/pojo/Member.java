9c444deb95240590ca86b241da8b3f161e56aace
Vishal Row Mysore <visrow@gmail.com>
2024-04-20 15:47:21 -0400

    image validation and selenim

--- src/main/java/org/example/pojo/Member.java
+++ src/main/java/org/example/pojo/Member.java
@@ -1,6 +1,7 @@
 package org.example.pojo;
 
 import com.fasterxml.jackson.databind.DatabindException;
+import com.t4a.annotations.Prompt;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,6 +16,7 @@ import java.util.Date;
 public class Member {
     private String id;
     private String name;
+    @Prompt(dateFormat = "ddMMyyyy")
     private Date membershipStart;
     private int booksLoaned;
     // Constructors, getters, and setters
